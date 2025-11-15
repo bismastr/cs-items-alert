47a0609065aa9185867abf6ced2d7210cf7b49f6
bismastr <bismasatrian@gmail.com>
2025-11-15 19:03:41 +0700

    feat: add price changes repository

--- internal/price/price.go
+++ internal/price/price.go
@@ -3,29 +3,47 @@ package price
 import (
 	"context"
 
+	"github.com/bismastr/cs-price-alert/internal/repository"
 	"github.com/bismastr/cs-price-alert/internal/timescale_repository"
 )
 
-type PriceRepository interface {
+type TimescaleRepository interface {
 	Get24HourPricesChanges(ctx context.Context) ([]timescale_repository.Get24HourPricesChangesRow, error)
 	InsertPrice(ctx context.Context, params timescale_repository.InsertPriceParams) error
 }
 
+type PostgresRepository interface {
+	GetItemByID(ctx context.Context, ids []int32) ([]repository.Item, error)
+}
+
 type PriceService struct {
-	repo PriceRepository
+	timescaleRepo TimescaleRepository
+	postgresRepo  PostgresRepository
 }
 
-type GetPriceChange24Hour struct {
-	ItemId    int32
-	ChangePct float64
+type GetPriceChange24HourResults struct {
+	ItemId          int32
+	ChangePct       float64
+	Name            string
+	OldSellPrice    int32
+	LatestSellPrice int32
 }
 
-func NewPriceService(repo PriceRepository) *PriceService {
-	return &PriceService{repo: repo}
+type InsertPriceParams struct {
+	ItemID       int32
+	SellPrice    int32
+	SellListings int32
+}
+
+func NewPriceService(timescaleRepo TimescaleRepository, postgresRepo PostgresRepository) *PriceService {
+	return &PriceService{
+		timescaleRepo: timescaleRepo,
+		postgresRepo:  postgresRepo,
+	}
 }
 
 func (s *PriceService) InsertItem(ctx context.Context, item timescale_repository.InsertPriceParams) error {
-	err := s.repo.InsertPrice(ctx, item)
+	err := s.timescaleRepo.InsertPrice(ctx, item)
 	if err != nil {
 		return err
 	}
@@ -33,18 +51,42 @@ func (s *PriceService) InsertItem(ctx context.Context, item timescale_repository
 	return nil
 }
 
-func (s *PriceService) GetLatestPriceByHashName(ctx context.Context) ([]GetPriceChange24Hour, error) {
-	prices, err := s.repo.Get24HourPricesChanges(ctx)
+func (s *PriceService) GetPriceChange24Hour(ctx context.Context) ([]GetPriceChange24HourResults, error) {
+	priceChanges, err := s.timescaleRepo.Get24HourPricesChanges(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	var result []GetPriceChange24Hour
-	for _, price := range prices {
-		changePct := float64((price.LatestSellPrice - price.OldSellPrice)) / float64(price.OldSellPrice) * 100
-		result = append(result, GetPriceChange24Hour{
-			ItemId:    price.ItemID,
-			ChangePct: changePct,
+	priceMap := make(map[int32]GetPriceChange24HourResults)
+	var itemdIds []int32
+	for _, priceChange := range priceChanges {
+		priceMap[priceChange.ItemID] = GetPriceChange24HourResults{
+			ItemId:          priceChange.ItemID,
+			ChangePct:       float64(priceChange.LatestSellPrice-priceChange.OldSellPrice) / float64(priceChange.OldSellPrice) * 100,
+			OldSellPrice:    priceChange.OldSellPrice,
+			LatestSellPrice: priceChange.LatestSellPrice,
+		}
+		itemdIds = append(itemdIds, priceChange.ItemID)
+	}
+
+	items, err := s.postgresRepo.GetItemByID(ctx, itemdIds)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []GetPriceChange24HourResults
+	for _, item := range items {
+		priceChange, exists := priceMap[item.ID]
+		if !exists {
+			continue
+		}
+
+		result = append(result, GetPriceChange24HourResults{
+			ItemId:          item.ID,
+			ChangePct:       priceChange.ChangePct,
+			Name:            item.Name,
+			OldSellPrice:    priceChange.OldSellPrice,
+			LatestSellPrice: priceChange.LatestSellPrice,
 		})
 	}
 
--- internal/price/price_test.go
+++ internal/price/price_test.go
@@ -4,58 +4,80 @@ import (
 	"context"
 	"testing"
 
+	"github.com/bismastr/cs-price-alert/internal/repository"
 	"github.com/bismastr/cs-price-alert/internal/timescale_repository"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-type MockPriceRepo struct {
+type MockTimescaleRepo struct {
 	mock.Mock
 }
 
-func (m *MockPriceRepo) Get24HourPricesChanges(ctx context.Context) ([]timescale_repository.Get24HourPricesChangesRow, error) {
+type MockPostgresRepo struct {
+	mock.Mock
+}
+
+func (m *MockPostgresRepo) GetItemByID(ctx context.Context, ids []int32) ([]repository.Item, error) {
+	args := m.Called(ctx, ids)
+	return args.Get(0).([]repository.Item), args.Error(1)
+}
+
+func (m *MockTimescaleRepo) Get24HourPricesChanges(ctx context.Context) ([]timescale_repository.Get24HourPricesChangesRow, error) {
 	args := m.Called(ctx)
 	return args.Get(0).([]timescale_repository.Get24HourPricesChangesRow), args.Error(1)
 }
 
-func (m *MockPriceRepo) InsertPrice(ctx context.Context, params timescale_repository.InsertPriceParams) error {
+func (m *MockTimescaleRepo) InsertPrice(ctx context.Context, params timescale_repository.InsertPriceParams) error {
 	args := m.Called(ctx, params)
 	return args.Error(0)
 }
 
-func TestGetLatestPriceByHashName_Success(t *testing.T) {
+func TestGetPriceChange24Hour_Success(t *testing.T) {
 	ctx := context.Background()
 
-	mockRepo := new(MockPriceRepo)
-	mockRepo.On("Get24HourPricesChanges", mock.Anything).Return([]timescale_repository.Get24HourPricesChangesRow{
+	mockTimescaleRepo := new(MockTimescaleRepo)
+	mockTimescaleRepo.On("Get24HourPricesChanges", mock.Anything).Return([]timescale_repository.Get24HourPricesChangesRow{
 		{ItemID: 1, LatestSellPrice: 1200, OldSellPrice: 1000},
 		{ItemID: 2, LatestSellPrice: 800, OldSellPrice: 1000},
 	}, nil)
 
-	service := NewPriceService(mockRepo)
+	mockPostgresRepo := new(MockPostgresRepo)
+	mockPostgresRepo.On("GetItemByID", mock.Anything, []int32{1, 2}).Return([]repository.Item{
+		{ID: 1, HashName: "item1"},
+		{ID: 2, HashName: "item2"},
+	}, nil)
 
-	result, err := service.GetLatestPriceByHashName(ctx)
+	service := NewPriceService(mockTimescaleRepo, mockPostgresRepo)
+
+	result, err := service.GetPriceChange24Hour(ctx)
 
 	assert.NoError(t, err)
 	assert.Len(t, result, 2)
 	assert.EqualValues(t, 20.0, result[0].ChangePct)
 	assert.EqualValues(t, -20.0, result[1].ChangePct)
 
-	mockRepo.AssertExpectations(t)
+	mockTimescaleRepo.AssertExpectations(t)
 }
 
-func TestGetLatestPriceByHashName_Error(t *testing.T) {
+func TestGetPriceChange24Hour_Error(t *testing.T) {
 	ctx := context.Background()
 
-	mockRepo := new(MockPriceRepo)
+	mockRepo := new(MockTimescaleRepo)
 	mockRepo.On("Get24HourPricesChanges", mock.Anything).Return(
 		[]timescale_repository.Get24HourPricesChangesRow(nil),
 		assert.AnError,
 	)
 
-	service := NewPriceService(mockRepo)
+	mockPostgresRepo := new(MockPostgresRepo)
+	mockPostgresRepo.On("GetItemByID", mock.Anything, []int32{1, 2}).Return([]repository.Item{
+		{ID: 1, HashName: "item1"},
+		{ID: 2, HashName: "item2"},
+	}, nil)
 
-	result, err := service.GetLatestPriceByHashName(ctx)
+	service := NewPriceService(mockRepo, mockPostgresRepo)
+
+	result, err := service.GetPriceChange24Hour(ctx)
 
 	assert.Error(t, err)
 	assert.Nil(t, result)
@@ -66,7 +88,8 @@ func TestGetLatestPriceByHashName_Error(t *testing.T) {
 func TestInsertItem_Success(t *testing.T) {
 	ctx := context.Background()
 
-	mockRepo := new(MockPriceRepo)
+	mockRepo := new(MockTimescaleRepo)
+	mockPostgresRepo := new(MockPostgresRepo)
 	params := timescale_repository.InsertPriceParams{
 		ItemID:       1,
 		SellPrice:    100,
@@ -75,7 +98,7 @@ func TestInsertItem_Success(t *testing.T) {
 
 	mockRepo.On("InsertPrice", ctx, params).Return(nil)
 
-	service := NewPriceService(mockRepo)
+	service := NewPriceService(mockRepo, mockPostgresRepo)
 
 	err := service.InsertItem(ctx, params)
 
@@ -87,7 +110,7 @@ func TestInsertItem_Success(t *testing.T) {
 func TestInsertItem_Error(t *testing.T) {
 	ctx := context.Background()
 
-	mockRepo := new(MockPriceRepo)
+	mockRepo := new(MockTimescaleRepo)
 	params := timescale_repository.InsertPriceParams{
 		ItemID:       1,
 		SellPrice:    100,
@@ -96,7 +119,9 @@ func TestInsertItem_Error(t *testing.T) {
 
 	mockRepo.On("InsertPrice", ctx, params).Return(assert.AnError)
 
-	service := NewPriceService(mockRepo)
+	mockPostgresRepo := new(MockPostgresRepo)
+
+	service := NewPriceService(mockRepo, mockPostgresRepo)
 
 	err := service.InsertItem(ctx, params)
 
